4fd692aae1eab790f408c3c86645781852a37c14
Ramesh Kumar Sah <rameshjarvis@live.com>
2021-05-25 14:24:21 -0700

    added data loader for wesad and oversampling method support

--- data_loader.py
+++ data_loader.py
@@ -3,10 +3,10 @@ import numpy as np
 import sys
 
 sys.path.append("../Python Scripts/")
-
 import utils as utl
 
-def create_dataset(stress, not_stress, path=True, reshape=True, train_test=True, balance_classes=False):
+
+def create_dataset(stress, not_stress, path=True, reshape=True, train_test=True, balance_classes=False, oversampling_method=None):
     """Create dataset given stress and not-stress data or path. 
     
     
@@ -16,6 +16,7 @@ def create_dataset(stress, not_stress, path=True, reshape=True, train_test=True,
     reshape -- whether to reshape the data as (n_samples, window_length, n_channels) (default True)
     train_test -- whether to split the data into train and test sets (default True)
     balance_classes -- whether to balance the data between the classes (default True)
+    oversampling_method -- initialized methods from imblearn.over_sampling such as RandomOverSampler, SMOTE, ADASYN
 
     """
     # if path load the datasets
@@ -37,6 +38,10 @@ def create_dataset(stress, not_stress, path=True, reshape=True, train_test=True,
         np.zeros(len(not_stress_segments), dtype=int)
     ], axis=0)
 
+    # if oversampling specified
+    if (oversampling_method != None) & (balance_classes == False):
+        x, y = oversampling_method.fit_resample(x, y)
+
     # reshape is instructed
     if reshape:
         x = x.reshape(-1, x.shape[1], 1)
@@ -47,3 +52,29 @@ def create_dataset(stress, not_stress, path=True, reshape=True, train_test=True,
         return x_tr, x_ts, y_tr, y_ts, utl.get_hot_labels(y_tr), utl.get_hot_labels(y_ts)
     else:
         return x, y
+
+
+def load_wesad_data(baseline, amusement, stressed, combine_amusement=False, reshape=True, train_test=True):
+    baseline_segments = utl.read_data(baseline)
+    stressed_segments = utl.read_data(stressed)
+    
+    if combine_amusement:
+        amusement_segments = utl.read_data(amusement)
+        X = np.concatenate([stressed_segments, baseline_segments, amusement_segments], axis = 0)
+
+        Y = np.concatenate([np.ones(stressed_segments.shape[0], dtype=int), 
+                        np.zeros(baseline_segments.shape[0] + amusement_segments.shape[0], dtype=int)])
+    else:
+        X = np.concatenate([stressed_segments, baseline_segments], axis = 0)
+
+        Y = np.concatenate([np.ones(stressed_segments.shape[0], dtype=int), 
+                        np.zeros(baseline_segments.shape[0], dtype=int)])
+
+    if reshape:
+        X = X.reshape(-1, X.shape[1], 1)
+
+    if train_test:
+        x_train, x_val, x_test, y_train, y_val, y_test = utl.split_into_train_val_test(X, Y, test_split=0.25)
+        return x_train, x_test, y_train, y_test, utl.get_hot_labels(y_train), utl.get_hot_labels(y_test)
+    else:
+        return X, Y
--- stress_models.py
+++ stress_models.py
@@ -24,17 +24,17 @@ def get_simple_cnn_model(input_shape, metrics, learning_rate):
         keras.layers.Dropout(rate = 0.3),
         
         keras.layers.Dense(units = 64, activation=tf.nn.relu),
-        keras.layers.Dense(units = 2, activation = tf.nn.softmax)
+        keras.layers.Dense(units = 1, activation = tf.nn.sigmoid)
     ])
     
-    temp_model.compile(loss = keras.losses.categorical_crossentropy, 
+    temp_model.compile(loss = keras.losses.BinaryCrossentropy(), 
                        optimizer = keras.optimizers.Adam(learning_rate = learning_rate), 
                       metrics = metrics)
     
     return temp_model
 
 
-def get_supervised_balance_model(input_shape, metrics, learning_rate):
+def get_supervised_balance_adarp_model(input_shape, metrics, learning_rate):
     """After hyperparameterization optimization the best set of hyperparameters were:
         1. batch_size = 147
         2. CNN1 filters = 100
@@ -66,16 +66,16 @@ def get_supervised_balance_model(input_shape, metrics, learning_rate):
         keras.layers.Dropout(rate = 0.3),
         
         keras.layers.Dense(units = 64, activation=tf.nn.relu),
-        keras.layers.Dense(units = 2, activation = tf.nn.softmax)
+        keras.layers.Dense(units = 1, activation = tf.nn.sigmoid)
     ])
     
-    temp_model.compile(loss = keras.losses.categorical_crossentropy, 
+    temp_model.compile(loss = keras.losses.BinaryCrossentropy(), 
                        optimizer = keras.optimizers.Adam(learning_rate = learning_rate), 
                       metrics = metrics)
     
     return temp_model
 
-def get_supervised_full_model(input_shape, metrics, learning_rate):
+def get_supervised_full_adarp_model(input_shape, metrics, learning_rate):
     """After hyperparameterization optimization the best set of hyperparameters were:
         1. Batch size = 100
         2. CNN1 filters = 250
@@ -107,11 +107,117 @@ def get_supervised_full_model(input_shape, metrics, learning_rate):
         keras.layers.Dropout(rate = 0.1),
         
         keras.layers.Dense(units = 64, activation=tf.nn.relu),
+        keras.layers.Dense(units = 1, activation = tf.nn.sigmoid)
+    ])
+    
+    temp_model.compile(loss = keras.losses.BinaryCrossentropy(), 
+                       optimizer = keras.optimizers.Adam(learning_rate = learning_rate), 
+                      metrics = metrics)
+    
+    return temp_model
+
+
+
+def get_transfer_wesad_model(input_shape, metrics, learning_rate):
+    """After hyperparameterization optimization the best set of hyperparameters were:
+        1. Batch size = 120
+        2. CNN1 filters = 100
+        3. CNN1 kernel size = 10
+        4. CNN2 filters = 50
+        5. CNN2 kernel size = 5
+        6. Dense1 units = 128
+        7. Dense2 units = 64
+        8. Dense3 units = 128
+        9. Dropout1 = 0.2
+        10. Dropout2 = 0.3
+        11. Learning rate = 0.0154
+        12. Optimizer = Adam
+        
+        For test loss of 0.2275
+        Return the CNN model
+    """
+    # temp_model = keras.Sequential([
+    #     keras.layers.Conv1D(filters=100, kernel_size = (10), strides = 1, activation = tf.nn.relu, 
+    #                         input_shape = input_shape, padding='same'),
+      
+    #     keras.layers.Conv1D(filters = 50, kernel_size = (5), strides = 1, 
+    #                         activation = tf.nn.relu, padding='same'),
+    #     keras.layers.GlobalMaxPool1D(),
+        
+    #     keras.layers.Dense(units = 128, activation = tf.nn.relu),
+    #     keras.layers.Dropout(rate = 0.2),
+
+    #     keras.layers.Dense(units = 64, activation = tf.nn.relu),
+    #     keras.layers.Dropout(rate = 0.3),
+        
+    #     keras.layers.Dense(units = 128, activation=tf.nn.relu, name='penultimate_layer'),
+    #     keras.layers.Dense(units = 2, activation = tf.nn.softmax)
+    # ])
+    
+    # temp_model.compile(loss = keras.losses.categorical_crossentropy, 
+    #                    optimizer = keras.optimizers.Adam(learning_rate = learning_rate), 
+    #                   metrics = metrics)
+
+    temp_model = keras.Sequential([
+        keras.layers.Conv1D(filters = 100, kernel_size = (5), strides = 1, activation = tf.nn.relu, 
+                            input_shape = input_shape),
+        
+        keras.layers.Conv1D(filters = 100, kernel_size = (10), strides = 1, activation = tf.nn.relu),
+        keras.layers.GlobalMaxPool1D(),
+        
+        keras.layers.Dense(units = 128, activation = tf.nn.relu),
+        keras.layers.Dropout(rate = 0.3),
+        
+        keras.layers.Dense(units = 64, activation = tf.nn.relu, name='penultimate_layer'),
+        keras.layers.Dropout(rate = 0.2),
         keras.layers.Dense(units = 2, activation = tf.nn.softmax)
     ])
     
     temp_model.compile(loss = keras.losses.categorical_crossentropy, 
                        optimizer = keras.optimizers.Adam(learning_rate = learning_rate), 
                       metrics = metrics)
+    
+    return temp_model
+
+
+def get_transfer_adarp_model(input_shape, metrics, learning_rate):
+    """After hyperparameterization optimization the best set of hyperparameters were:
+        1. Batch size = 106
+        2. CNN1 filters = 250
+        3. CNN1 kernel size = 2
+        4. CNN2 filters = 100
+        5. CNN2 kernel size = 10
+        6. Dense1 units = 64
+        7. Dense2 units = 128
+        8. Dense3 units = 128
+        9. Dropout1 = 0.1
+        10. Dropout2 = 0.1
+        11. Learning rate = 0.002046
+        12. Optimizer = Adam
+        
+        For test loss of 0.5263
+        Return the CNN model
+    """
+    temp_model = keras.Sequential([
+        keras.layers.Conv1D(filters=250, kernel_size=(2), strides=1, activation=tf.nn.relu, 
+                            input_shape=input_shape, padding='same'),
+      
+        keras.layers.Conv1D(filters=100, kernel_size=(10), strides=1, 
+                            activation=tf.nn.relu, padding='same'),
+        keras.layers.GlobalMaxPool1D(),
+        
+        keras.layers.Dense(units=64, activation=tf.nn.relu),
+        keras.layers.Dropout(rate=0.1),
+
+        keras.layers.Dense(units=128, activation=tf.nn.relu),
+        keras.layers.Dropout(rate=0.1),
+        
+        keras.layers.Dense(units=128, activation=tf.nn.relu),
+        keras.layers.Dense(units=1, activation=tf.nn.sigmoid)
+    ])
+    
+    temp_model.compile(loss=keras.losses.BinaryCrossentropy(), 
+                       optimizer=keras.optimizers.Adam(learning_rate = learning_rate), 
+                      metrics=metrics)
     
     return temp_model
